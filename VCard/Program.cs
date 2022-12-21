42415a264f1528134a3fcf6a68b502f00d36e3e6
Mranar <anarbalacayev@gmail.com>
2022-12-21 10:26:05 +0400

    Created HomeWork5

---
+++ VCard/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using System.Text.Json;
+using VCard.Models;
+public class Program
+{
+ 
+    public static void Main()
+    {
+
+        using (var vCard = new HttpClient())
+        {
+            var endpoint = new Uri("https://randomuser.me/api?results=");
+            
+
+            var result = vCard.GetAsync(endpoint).Result;
+            var json = result.Content.ReadAsStringAsync().Result;
+
+            //var jsonseria = JsonSerializer.Deserialize<VCards>(json);
+
+
+
+
+            //foreach (PropertyInfo property in jsonseria.GetType().GetProperties())
+            //{
+            //    Console.WriteLine(property.GetValue(jsonseria , null));
+            //}
+
+
+            List<VCards> vCards = JsonConvert.DeserializeObject<List<VCards>>(json);
+
+            Console.WriteLine(vCards.ToString());
+
+
+        }
+
+    }
+}
